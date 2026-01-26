f466fb1b88e9818781947a5a5b213994da4926ed
0930yuyaikeda <0930arnaud@gmail.com>
2026-01-26 11:19:02 +0700

    yuyaikeda 20250126 細かい修正

--- src/js/text-animation01.js
+++ src/js/text-animation01.js
@@ -3,8 +3,8 @@
 // 一度のみ実行
 window.setTimeout(function(){
 
-    const text1 = document.getElementById('text1');
-    const text2 = document.getElementById('text2');
+    const text1 = document.getElementById('ta01-text1');
+    const text2 = document.getElementById('ta01-text2');
     text1.classList.add('is-active');
     text2.classList.add('is-active');
 
