81eb62f71468877eb1c7f34d16ce7063ec945add
0930yuyaikeda <0930arnaud@gmail.com>
2026-02-01 01:12:17 +0700

    yuyaikeda 20260131 ３分でわかるPFを追加

---
+++ src/js/quick.js
@@ -0,0 +1,8 @@
+window.setTimeout(function(){
+
+    const text1 = document.getElementById('ta01-main-text1');
+    const text2 = document.getElementById('ta01-main-text2');
+    text1.classList.add('is-active');
+    text2.classList.add('is-active');
+
+}, 100);
